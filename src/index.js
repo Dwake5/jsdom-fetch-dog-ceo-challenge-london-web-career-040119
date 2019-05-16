08cdbf1a8b468a49c1adb0b840a80483a97f5de8
Dwake5 <wakeling101@hotmail.com>
2019-05-16 09:37:12 +0100

    Challenges 1 - 3 completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-console.log('%c HI', 'color: firebrick')
+function fetchDogs () {
+ fetch("https://dog.ceo/api/breeds/image/random/4")
+ .then(resp => resp.json())
+ .then(json => addImages(json.message))
+}
+
+function addImages (urls) {
+    const container = document.querySelector('#dogsImg')
+    urls.forEach(url => {
+        
+        container.appendChild(addImage(url))
+    });
+}
+
+function addImage(dog) {
+    let img = document.createElement('img')
+    img.src=dog
+        return img
+}
+
+// End of fetch dogs - tested and working
+
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+function fetchBreeds () {
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(json => addBreeds(json.message))
+}
+
+function addBreeds (breeds) {
+    const container = document.querySelector('#dog-breeds')
+    Object.keys(breeds).forEach(breed => {
+        container.appendChild(addBreed(breed))
+    });
+}
+
+function addBreed (dog) {
+    let breed = document.createElement('li')
+    breed.innerHTML = dog
+    breed.addEventListener('click', function(event){
+        event.target.style.color = "red" 
+    })
+    return breed
+}
+
+fetchBreeds()
+fetchDogs()
